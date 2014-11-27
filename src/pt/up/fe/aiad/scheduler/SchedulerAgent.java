a00cc7393ff6b55aa88ede314b196560fba2f1ce
Luis <luisccleto@gmail.com>
2014-11-27 10:50:16 +0000

    Add agent setup behaviour. Agents can now find eachother as they register in the platform

--- src/pt/up/fe/aiad/scheduler/SchedulerAgent.java
+++ src/pt/up/fe/aiad/scheduler/SchedulerAgent.java
@@ -3,16 +3,19 @@ package pt.up.fe.aiad.scheduler;
 
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import jade.proto.SubscriptionInitiator;
+import jade.wrapper.AgentController;
+import jade.wrapper.StaleProxyException;
 import pt.up.fe.aiad.scheduler.agentbehaviours.ABTBehaviour;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeSet;
+import java.util.*;
 
 
 public class SchedulerAgent extends Agent {
@@ -20,21 +23,32 @@ public class SchedulerAgent extends Agent {
         ABT
     }
 
-    public ArrayList<ScheduleEvent> _events; /** List of all the events this agent is participating in */
-    public Map<String, ArrayList<AID>> _participants; /** For each of the events this agent is participating in, lists other participants */
+    /**
+     * List of all the events this agent is participating in
+     */
+    public ArrayList<ScheduleEvent> _events;
+    /**
+     * For each of the events this agent is participating in, lists other participants
+     */
+    public Map<String, ArrayList<AID>> _participants;
 
-    private final Type _agentType;
-    public SchedulerAgent(Type agentType) {
-        _agentType = agentType;
+    public Set<AID> _allAgents;
+
+    private Type _agentType;
+
+    public static void main(String[] args) {
+        String[] ar = {"-gui"};
+        jade.Boot.main(ar);
+    }
+
+    public SchedulerAgent() {
         _events = new ArrayList<ScheduleEvent>();
         _participants = new HashMap<String, ArrayList<AID>>();
-
+        _allAgents = new HashSet<AID>();
     }
 
     @Override
     public void setup() {
-        //TODO Create and show the GUI
-
         System.out.println("Hello. I, agent " + getAID().getName() + " am alive now.");
         DFAgentDescription dfd = new DFAgentDescription();
         dfd.setName(getAID());
@@ -43,13 +57,32 @@ public class SchedulerAgent extends Agent {
         sd.setName("iScheduler");
         dfd.addServices(sd);
         try {
+            DFAgentDescription template = new DFAgentDescription();
+
+            addBehaviour(new SubscriptionInitiator(this, DFService.createSubscriptionMessage(this, getDefaultDF(), template, null)) {
+                protected void handleInform(ACLMessage inform) {
+                    try {
+                        DFAgentDescription[] dfds = DFService.decodeNotification(inform.getContent());
+
+                        for (DFAgentDescription dfd1 : dfds) {
+                            if (!dfd1.getName().toString().equals(getAID().toString())) {
+                                _allAgents.add(dfd1.getName());
+                                System.out.println("I, agent " + getAID().getName() + ", have found agent " + dfd1.getName().getName() + ".");
+                            }
+                        }
+                    } catch (FIPAException fe) {
+                        fe.printStackTrace();
+                    }
+                }
+            });
+
             DFService.register(this, dfd);
-        }
-        catch (FIPAException fe) {
+        } catch (FIPAException fe) {
             fe.printStackTrace();
         }
 
 
+        /* This must be done only after SETUP phase ends
         switch (_agentType) {
             case ABT:
                 addBehaviour(new ABTBehaviour());
@@ -59,6 +92,7 @@ public class SchedulerAgent extends Agent {
                 doDelete();
                 break;
         }
+        */
     }
 
     @Override
@@ -66,8 +100,7 @@ public class SchedulerAgent extends Agent {
         // Deregister from the yellow pages
         try {
             DFService.deregister(this);
-        }
-        catch (FIPAException fe) {
+        } catch (FIPAException fe) {
             fe.printStackTrace();
         }
         //TODO Close the GUI if necessary
---
+++ src/pt/up/fe/aiad/scheduler/agentbehaviours/SetupBehaviour.java
@@ -0,0 +1,12 @@
+package pt.up.fe.aiad.scheduler.agentbehaviours;
+
+import jade.core.behaviours.CyclicBehaviour;
+
+public class SetupBehaviour extends CyclicBehaviour {
+
+    @Override
+    public void action() {
+
+    }
+
+}
