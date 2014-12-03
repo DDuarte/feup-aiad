cb13742745ccd8335511a78c8196cfead46ee5d5
Luis <luisccleto@gmail.com>
2014-12-03 19:24:17 +0000

    Create Constraint interface

---
+++ src/pt/up/fe/aiad/scheduler/constraints/ScheduleConstraint.java
@@ -0,0 +1,7 @@
+package pt.up.fe.aiad.scheduler.constraints;
+
+import pt.up.fe.aiad.utils.TimeInterval;
+
+public interface ScheduleConstraint {
+    public boolean isSatisfiedBy(TimeInterval ti);
+}
