6a3cd61751a69190eb4e485269b0acb3b0ba425a
Nayd <dnpd.dd@gmail.com>
2014-12-14 03:06:14 +0000

    Re-enable StatisticsTest

--- src/pt/up/fe/aiad/tests/StatisticsTest.java
+++ src/pt/up/fe/aiad/tests/StatisticsTest.java
@@ -1,37 +1,37 @@
 package pt.up.fe.aiad.tests;
 
+import org.junit.Test;
+import pt.up.fe.aiad.scheduler.Statistics;
+
+import static org.junit.Assert.assertEquals;
+
 public class StatisticsTest {
-/*
+
     @Test
     public void testSentMessage() throws Exception {
         Statistics s = new Statistics();
 
-        s.sentMessage("A", "X");
-        s.sentMessage("A", "X");
-        s.sentMessage("B", "X");
-        s.sentMessage("B", "Y");
+        s.sentMessage("X");
+        s.sentMessage("X");
+        s.sentMessage("X");
+        s.sentMessage("Y");
 
-        assertEquals(2, s.getSentMessages("A", "X"));
-        assertEquals(0, s.getSentMessages("A", "Y"));
-        assertEquals(1, s.getSentMessages("B", "X"));
-        assertEquals(1, s.getSentMessages("B", "Y"));
-        assertEquals(0, s.getSentMessages("C", "X"));
+        assertEquals(3, s.getSentMessages("X"));
+        assertEquals(1, s.getSentMessages("Y"));
+        assertEquals(0, s.getSentMessages("Z"));
     }
 
     @Test
     public void testReceivedMessage() throws Exception {
         Statistics s = new Statistics();
 
-        s.receivedMessage("A", "X");
-        s.receivedMessage("A", "X");
-        s.receivedMessage("B", "X");
-        s.receivedMessage("B", "Y");
+        s.receivedMessage("X");
+        s.receivedMessage("X");
+        s.receivedMessage("X");
+        s.receivedMessage("Y");
 
-        assertEquals(2, s.getReceivedMessages("A", "X"));
-        assertEquals(0, s.getReceivedMessages("A", "Y"));
-        assertEquals(1, s.getReceivedMessages("B", "X"));
-        assertEquals(1, s.getReceivedMessages("B", "Y"));
-        assertEquals(0, s.getReceivedMessages("C", "X"));
+        assertEquals(3, s.getReceivedMessages("X"));
+        assertEquals(1, s.getReceivedMessages("Y"));
+        assertEquals(0, s.getReceivedMessages("Z"));
     }
-    */
 }
